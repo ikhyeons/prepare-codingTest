50d64385ad7c73c9803b6d1b6b5610b9b280ba9c
ikhyeons <skantrkwl789@naver.com>
2023-06-12 02:53:52 +0900

    Update 2023.06.12

---
+++ runner.js
@@ -0,0 +1,35 @@
+let Tarr = [3, 7];
+
+let result = "";
+let makeSolve = [1];
+let op = [" ", "+", "-"];
+
+function dfs(arr, depth) {
+  if (depth == arr.length) {
+    let answer = eval(makeSolve.join("").split(" ").join(""));
+
+    if (answer === 0) {
+      result += makeSolve.join("");
+      result += "\n";
+    }
+    return;
+  }
+
+  for (let oper of op) {
+    makeSolve.push(oper);
+    makeSolve.push(arr[depth]);
+    dfs(arr, depth + 1);
+    makeSolve.pop();
+    makeSolve.pop();
+  }
+}
+
+for (let Test of Tarr) {
+  let number = []; // 숫자 배열 [1, 2, 3, 4, 5, 6, 7]
+  for (let i = 1; i <= Test; i++) number.push(i);
+
+  dfs(number, 1);
+  result += "\n";
+}
+
+console.log(result);
